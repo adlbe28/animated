a093ef6d5bb8aee68ad8d23e402a3fa101e1aed6
ahmadadlbe <ahmadadlbe@gmail.com>
2024-01-25 14:13:38 +0300

    add Animated Position

---
+++ lib/home/view/widget/custom_animated_position.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomAnimatedPosition extends StatefulWidget {
+  const CustomAnimatedPosition({super.key});
+
+  @override
+  State<CustomAnimatedPosition> createState() => _CustomAnimatedPositionState();
+}
+
+class _CustomAnimatedPositionState extends State<CustomAnimatedPosition> {
+  double top = 0;
+  double right = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(color: Colors.amber, width: 400, height: 400),
+            AnimatedPositioned(
+              duration: const Duration(seconds: 1),
+              top: top,
+              right: right,
+              child: Container(
+                color: Colors.blue,
+                height: 100,
+                width: 100,
+              ),
+            )
+          ],
+        ),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                top = 100;
+                right = 200;
+              });
+            },
+            child: const Text("Animat"))
+      ],
+    );
+  }
+}
