e476b88646bd0b95793c238d28f0c0e5076694e7
ahmadadlbe <ahmadadlbe@gmail.com>
2024-02-06 11:59:34 +0300

    final Edit on Foo Transition

--- lib/home/view/foo_transition_widget.dart
+++ lib/home/view/foo_transition_widget.dart
@@ -9,24 +9,54 @@ class FooTransitionWidgetScreen extends StatefulWidget {
 }
 
 class _FooTransitionWidgetScreenState extends State<FooTransitionWidgetScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animatedController;
+    with TickerProviderStateMixin {
+  late AnimationController _greenController;
+  late AnimationController _redController;
   late Animation<AlignmentGeometry> _greencircle;
   late Animation<AlignmentGeometry> _redCircle;
   @override
   void initState() {
-    _animatedController = AnimationController(
+    _greenController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+      reverseDuration: const Duration(seconds: 2),
+    );
+    _redController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 2),
       reverseDuration: const Duration(seconds: 2),
     );
     _redCircle = Tween<AlignmentGeometry>(
             begin: Alignment.topCenter, end: Alignment.bottomCenter)
-        .animate(_animatedController);
+        .animate(
+            CurvedAnimation(parent: _redController, curve: Curves.easeInOut));
     _greencircle = Tween<AlignmentGeometry>(
             begin: Alignment.centerLeft, end: Alignment.centerRight)
-        .animate(_animatedController);
+        .animate(_greenController);
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _greenController.dispose();
+    _redController.dispose();
+    super.dispose();
+  }
+
+  void _ListenercontrollerStatus(animationStatus) {
+    print("=============$animationStatus===========");
+    if ((_greenController.status == AnimationStatus.dismissed) &&
+        (_redController.status == AnimationStatus.completed)) {
+      _greenController.forward();
+    }
+  }
+
+  void _ListenerControllerValue() {
+    print("=============${_redController.value} ===========");
+    if ((_redController.value <= 0.5) && (_greenController.value == 1)) {
+      _greenController.reverse();
+    }
   }
 
   @override
@@ -57,34 +87,58 @@ class _FooTransitionWidgetScreenState extends State<FooTransitionWidgetScreen>
           ),
           const Spacer(),
           Wrap(
+            spacing: 10,
             children: [
               ElevatedButton(
                   onPressed: () {
-                    _animatedController.forward();
+                    _redController.forward();
+                    // _redController.addStatusListener(_ListenercontrollerStatus);
                   },
                   child: const Text("Forward")),
               ElevatedButton(
                   onPressed: () {
-                    _animatedController.reverse();
+                    _redController.reverse();
+                    // _redController.addListener(_ListenerControllerValue);
                   },
                   child: const Text("Reverse")),
               ElevatedButton(
                   onPressed: () {
-                    _animatedController.stop();
+                    _redController.stop();
                   },
                   child: const Text("Stop")),
               ElevatedButton(
                   onPressed: () {
-                    _animatedController.repeat();
+                    _redController.repeat();
                   },
                   child: const Text("Repeat(revers=False)")),
               ElevatedButton(
                   onPressed: () {
-                    _animatedController.repeat(
+                    _redController.repeat(
                       reverse: true,
                     );
                   },
                   child: const Text("Repeat (reverse = True)")),
+              ElevatedButton(
+                  onPressed: () {
+                    _redController
+                        .removeStatusListener(_ListenercontrollerStatus);
+                  },
+                  child: const Text("remove Status Listener ")),
+              ElevatedButton(
+                  onPressed: () {
+                    _redController.removeListener(_ListenerControllerValue);
+                  },
+                  child: const Text("remove value Listener")),
+              ElevatedButton(
+                  onPressed: () {
+                    _redController.addStatusListener(_ListenercontrollerStatus);
+                  },
+                  child: const Text("add Status Listener ")),
+              ElevatedButton(
+                  onPressed: () {
+                    _redController.addListener(_ListenerControllerValue);
+                  },
+                  child: const Text("add value Listener")),
             ],
           )
         ],
